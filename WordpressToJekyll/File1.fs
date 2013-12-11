70701c88403bb7bff7053a6aac47fd154581b52c
Andrea <roundcrisis@gmail.com>
2013-12-11 22:08:28 +0000

    Initial stab, trying to find where things go, not doing all that well

---
+++ WordpressToJekyll/File1.fs
@@ -0,0 +1,11 @@
+﻿module File1
+
+open System.Xml.Linq
+open FSharp.Data
+
+
+    type ReadSample() = 
+        type posts = FSharp.Data.XmlProvider<"roundcrisis-wordpress.xml">
+        let  sample =posts.GetSample()
+        sprintf sample
+    
